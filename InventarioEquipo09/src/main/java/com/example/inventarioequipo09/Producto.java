589d5580eeae2c2180410cc4afe03307407dbd8a
angelitohernandez <20253ds082@utez.edu.mx>
2026-04-06 22:04:47 -0600

    Creacion de la clase producto

---
+++ InventarioEquipo09/src/main/java/com/example/inventarioequipo09/Producto.java
@@ -0,0 +1,61 @@
+package com.example.inventarioequipo09;
+
+public class Producto {
+    private String nombre;
+    private String codigo;
+    private double precio;
+    private int stock;
+    private String categoria;
+
+    public Producto(String codigo, String nombre, double precio, int stock, String categoria) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+    }
+
+    //Creacion de GET
+    public String getCodigo() {
+        return this.codigo;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    //Creacion del SET
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+
+    public String toString() {
+        return this.codigo + "," + this.nombre + "," + this.precio + "," + this.stock + "," + this.categoria;
+    }
+}
+
