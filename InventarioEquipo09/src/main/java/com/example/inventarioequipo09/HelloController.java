4ea288301fc86c22452fe3071e16f965d58b1b3d
Sebastian-CR7 <20253ds076@utez.edu.mx>
2026-04-07 22:35:17 -0600

    Funcionalidad de botones

--- InventarioEquipo09/src/main/java/com/example/inventarioequipo09/HelloController.java
+++ InventarioEquipo09/src/main/java/com/example/inventarioequipo09/HelloController.java
@@ -47,5 +47,61 @@ public class HelloController {
             }
         });
     }
+    @FXML
+    protected void onGuardarClick() {
+        try {
+            String codigo = txtCodigo.getText();
+
+            boolean yaExiste = masterData.stream()
+                    .anyMatch(p -> p.getCodigo().equalsIgnoreCase(codigo));
+
+
+            if (txtCodigo.isEditable() && yaExiste) {
+                throw new Exception("El código '" + codigo + "' ya está registrado. Use otro.");
+            }
+
+            if (!txtCodigo.isEditable()) {
+                Producto sel = tablaProductos.getSelectionModel().getSelectedItem();
+                if (sel != null) {
+                    sel.setNombre(txtNombre.getText());
+                    sel.setPrecio(Double.parseDouble(txtPrecio.getText()));
+                    sel.setStock(Integer.parseInt(txtStock.getText()));
+                    sel.setCategoria(txtCategoria.getText());
+                }
+            } else {
+                masterData.add(new Producto(codigo, txtNombre.getText(),
+                        Double.parseDouble(txtPrecio.getText()),
+                        Integer.parseInt(txtStock.getText()),
+                        txtCategoria.getText()));
+            }
+
+            service.guardar(masterData);
+            tablaProductos.refresh();
+            onLimpiarClick();
+
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error de Validación");
+            alert.setHeaderText(null);
+            alert.setContentText("Faltan rellenar campos o codigo duplicado.");
+            alert.showAndWait();
+        }
+    }
+    @FXML
+    protected void onEliminarClick() {
+        Producto sel = tablaProductos.getSelectionModel().getSelectedItem();
+        if (sel != null) {
+            masterData.remove(sel);
+            service.guardar(masterData);
+            onLimpiarClick();
+        }
+    }
+
+    @FXML
+    protected void onLimpiarClick() {
+        txtCodigo.clear(); txtCodigo.setEditable(true);
+        txtNombre.clear(); txtPrecio.clear(); txtStock.clear(); txtCategoria.clear();
+        tablaProductos.getSelectionModel().clearSelection();
+    }
 
 }
